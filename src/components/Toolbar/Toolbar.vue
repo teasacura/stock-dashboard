3750209172a608c1b0bba0bdbb7a2c25d95aa99b
Eva Leake <eva.leake@reconstructinc.com>
2022-08-16 15:56:44 -0700

    WIP - create new components

---
+++ src/components/Toolbar/Toolbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <Dropdown 
+      ref="month-dropdown"
+      :selected-index="month"
+      :drop-down-options="monthDropDownOptions"
+      @updateOption="emitMonthUpdate"
+      />
+  </div>
+  <!-- <Dropdown ref="year-dropdown"/> -->
+</template>
+
+<script>
+
+import Dropdown from "./components/Dropdown.vue";
+import { Months } from "../../constants.js";
+
+export default {
+  name: "Toolbar",
+  components: {
+    Dropdown
+  },
+  props: {
+    month: {
+      type: Number,
+      required: false
+    }
+  },
+  data() {
+    return {
+        monthDropDownOptions: [],
+        yearDropDownOptions: []
+    }
+  },
+  computed: {
+  },
+  mounted() {
+  },
+  created() {
+    this.monthDropDownOptions = Months.map((monthName, index) => {
+      return {
+        name: monthName,
+        number: index
+      }
+    });
+  },
+  methods: {
+    emitMonthUpdate(option) {
+      this.$emit('monthUpdateOption', option);
+    }
+  }
+}
+</script>
+
+<style scoped></style>
